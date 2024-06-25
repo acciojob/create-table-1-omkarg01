2c6f2ca250f207971fcde0bd519e82903899d557
Omkar Gujja <67428719+omkarg01@users.noreply.github.com>
2024-06-25 13:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+const btn = document.querySelector('input');
+
+
 function insert_Row() {
     //Write your code here
-  
-  
+    //your JS code here. If required.
+    var table = document.getElementById("sampleTable");
+    var row = table.insertRow(0); // Add a row to the end of the table
+    var rowCount = table.rows.length;
+    console.log(rowCount);
+
+    // Insert new cells (<td> elements)
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    // Add text to the new cells
+    cell1.innerHTML = "New Cell1"
+    cell2.innerHTML = "New Cell2"
+
+    // title.value = "";
+    // author.value = "";
+    // isbn.value = "";
 }
